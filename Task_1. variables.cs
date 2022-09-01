250078ddb040307e152e97d3a50f7606669ef0c7
DmitryMedS <110179038+DmitryMedS@users.noreply.github.com>
2022-09-01 06:17:10 +0300

    ...
    ...

---
+++ Task_1. variables.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 38;
+            int c = (31 – 5 * a) / b;
+             Console.WriteLine(c);
+
+
+        }
+    }          
+}
