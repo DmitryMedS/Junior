36b5f76e4695e3688046bca9a4950f2a4c6f0efa
DmitryMedS <110179038+DmitryMedS@users.noreply.github.com>
2022-09-03 13:11:31 +0300

    Add files via upload

--- Lesson 2,converter.cs
+++ Lesson 2,converter.cs
@@ -33,115 +33,123 @@ namespace ConsoleApp7
             Console.WriteLine("количество юаней");
 
             cny = Convert.ToSingle(Console.ReadLine());
-
-            Console.WriteLine("Приветствую вас в нашем обменнике...");
+            bool control = true;
+            while (control == true)
+            {
+                Console.WriteLine("Приветствую вас в нашем обменнике...");
             Console.WriteLine("1 - Обмен долларов на рубли");
             Console.WriteLine("2 - Обмен рублей на доллары");
             Console.WriteLine("3 - Обмен евро на рубли");
             Console.WriteLine("4 - Обмен рублей на евро");
             Console.WriteLine("5 - Обмен юаней на рубли");
             Console.WriteLine("6 - Обмен рублей на юани");
+            Console.WriteLine("7 - Выход");
             userInput = Convert.ToInt32(Console.ReadLine());
             Console.Clear();
-            switch (userInput)
-            {
-                case 1:
-                    Console.WriteLine("Обмен долларов на рубли");
-                    Console.WriteLine("Сколько хотите обменять?");
-                    currencyCount = Convert.ToSingle(Console.ReadLine());
-                    if (usd >= currencyCount)
-                    {
-                        usd -= currencyCount;            cny -= currencyCount;
-                                                           rub += currencyCount * cnyToRub;
-                        rub += currencyCount * usdToRub;
-                    }
-                    else
-                    {
-                        Console.WriteLine(" Недопустимое кол-во долларов.");
-                    }
-                    Console.WriteLine("Ваш баланс " + rub + " рублей " + usd + " долларов");
-                    
-                    break;
-                case 2:
-                    Console.WriteLine("Обмен рублей на доллары");
-                    Console.WriteLine("Сколько хотите обменять?");
-                    currencyCount = Convert.ToSingle(Console.ReadLine());
-                    if (rub >= currencyCount)
-                    {
-                        rub -= currencyCount;
-                        usd += currencyCount /rubToUsd;
-                    }
-                    else
-                    {
-                        Console.WriteLine(" Недопустимое кол-во рублей.");
-                    }
-                    Console.WriteLine("Ваш баланс " + rub + " рублей " + usd + " долларов");
+                switch (userInput)
+                {
+                    case 1:
+                        Console.WriteLine("Обмен долларов на рубли");
+                        Console.WriteLine("Сколько хотите обменять?");
+                        currencyCount = Convert.ToSingle(Console.ReadLine());
+                        if (usd >= currencyCount)
+                        {
+                            usd -= currencyCount; cny -= currencyCount;
+                            rub += currencyCount * cnyToRub;
+                            rub += currencyCount * usdToRub;
+                        }
+                        else
+                        {
+                            Console.WriteLine(" Недопустимое кол-во долларов.");
+                        }
+                        Console.WriteLine("Ваш баланс " + rub + " рублей " + usd + " долларов");
 
-                    break;
-                case 3:
-                    Console.WriteLine("Обмен евро на рубли");
-                    Console.WriteLine("Сколько хотите обменять?");
-                    currencyCount = Convert.ToSingle(Console.ReadLine());
-                    if (euro >= currencyCount)
-                    {
-                        euro -= currencyCount;
-                        rub += currencyCount * euroToRub;
-                    }
-                    else
-                    {
-                        Console.WriteLine(" Недопустимое кол-во долларов.");
-                    }
-                    Console.WriteLine("Ваш баланс " + rub + " рублей " + euro + " евро");
+                        break;
+                    case 2:
+                        Console.WriteLine("Обмен рублей на доллары");
+                        Console.WriteLine("Сколько хотите обменять?");
+                        currencyCount = Convert.ToSingle(Console.ReadLine());
+                        if (rub >= currencyCount)
+                        {
+                            rub -= currencyCount;
+                            usd += currencyCount / rubToUsd;
+                        }
+                        else
+                        {
+                            Console.WriteLine(" Недопустимое кол-во рублей.");
+                        }
+                        Console.WriteLine("Ваш баланс " + rub + " рублей " + usd + " долларов");
 
-                    break;
-                case 4:
-                    Console.WriteLine("Обмен рублей на евро");
-                    Console.WriteLine("Сколько хотите обменять?");
-                    currencyCount = Convert.ToSingle(Console.ReadLine());
-                    if (rub >= currencyCount)
-                    {
-                        rub -= currencyCount;
-                        euro+= currencyCount / rubToEuro;
-                    }
-                    else
-                    {
-                        Console.WriteLine(" Недопустимое кол-во рублей.");
-                    }
-                    Console.WriteLine("Ваш баланс " + rub + " рублей " + euro + " евро");
+                        break;
+                    case 3:
+                        Console.WriteLine("Обмен евро на рубли");
+                        Console.WriteLine("Сколько хотите обменять?");
+                        currencyCount = Convert.ToSingle(Console.ReadLine());
+                        if (euro >= currencyCount)
+                        {
+                            euro -= currencyCount;
+                            rub += currencyCount * euroToRub;
+                        }
+                        else
+                        {
+                            Console.WriteLine(" Недопустимое кол-во долларов.");
+                        }
+                        Console.WriteLine("Ваш баланс " + rub + " рублей " + euro + " евро");
 
-                    break;
-                case 5:
-                    Console.WriteLine("Обмен юаней на рубли");
-                    Console.WriteLine("Сколько хотите обменять?");
-                    currencyCount = Convert.ToSingle(Console.ReadLine());
-                    if (cny >= currencyCount)
-                    {
-                        cny -= currencyCount;
-                        rub += currencyCount * cnyToRub;
-                    }
-                    else
-                    {
-                        Console.WriteLine(" Недопустимое кол-во долларов.");
-                    }
-                    Console.WriteLine("Ваш баланс " + rub + " рублей " + cny + " юаней");
+                        break;
+                    case 4:
+                        Console.WriteLine("Обмен рублей на евро");
+                        Console.WriteLine("Сколько хотите обменять?");
+                        currencyCount = Convert.ToSingle(Console.ReadLine());
+                        if (rub >= currencyCount)
+                        {
+                            rub -= currencyCount;
+                            euro += currencyCount / rubToEuro;
+                        }
+                        else
+                        {
+                            Console.WriteLine(" Недопустимое кол-во рублей.");
+                        }
+                        Console.WriteLine("Ваш баланс " + rub + " рублей " + euro + " евро");
 
-                    break;
-                case 6:
-                    Console.WriteLine("Обмен рублей на юани");
-                    Console.WriteLine("Сколько хотите обменять?");
-                    currencyCount = Convert.ToSingle(Console.ReadLine());
-                    if (rub >= currencyCount)
-                    {
-                        rub -= currencyCount;
-                        cny += currencyCount / rubToCny;
-                    }
-                    else
-                    {
-                        Console.WriteLine(" Недопустимое кол-во рублей.");
-                    }
-                    Console.WriteLine("Ваш баланс " + rub + " рублей " + cny + " юаней");
+                        break;
+                    case 5:
+                        Console.WriteLine("Обмен юаней на рубли");
+                        Console.WriteLine("Сколько хотите обменять?");
+                        currencyCount = Convert.ToSingle(Console.ReadLine());
+                        if (cny >= currencyCount)
+                        {
+                            cny -= currencyCount;
+                            rub += currencyCount * cnyToRub;
+                        }
+                        else
+                        {
+                            Console.WriteLine(" Недопустимое кол-во долларов.");
+                        }
+                        Console.WriteLine("Ваш баланс " + rub + " рублей " + cny + " юаней");
 
-                    break;
+                        break;
+                    case 6:
+                        Console.WriteLine("Обмен рублей на юани");
+                        Console.WriteLine("Сколько хотите обменять?");
+                        currencyCount = Convert.ToSingle(Console.ReadLine());
+                        if (rub >= currencyCount)
+                        {
+                            rub -= currencyCount;
+                            cny += currencyCount / rubToCny;
+                        }
+                        else
+                        {
+                            Console.WriteLine(" Недопустимое кол-во рублей.");
+                        }
+                        Console.WriteLine("Ваш баланс " + rub + " рублей " + cny + " юаней");
+
+                        break;
+                    case 7:
+                        control = false;
+                        break;
+
+                }
             }
 
         }
