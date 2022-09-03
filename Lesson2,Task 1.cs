f4ac7e66aef51eff6411a7f4d1875943430d1f54
DmitryMedS <110179038+DmitryMedS@users.noreply.github.com>
2022-09-03 06:10:54 +0300

    //
    //

---
+++ Lesson2,Task 1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2.Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Дмитрий";
+            Console.WriteLine("Сколько раз мне повторить твое имя");
+            int countWrite;
+            int canWorking = 0;
+            countWrite = Convert.ToInt32(Console.ReadLine());
+            while (canWorking<countWrite)
+            {
+                canWorking++;
+                Console.WriteLine(name);
+            }
+
+            
+        }
+    }
+}
