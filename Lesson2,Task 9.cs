7855822fd22e3e0efe7d8c9375c6058d2f235a50
DmitryMedS <110179038+DmitryMedS@users.noreply.github.com>
2022-09-04 07:14:04 +0300

    ,,,

---
+++ Lesson2,Task 9.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2.Task9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            int minNumber = 0;
+            int maxNumber = 28;
+            int value = rand.Next(minNumber, maxNumber);
+            int count = 0;
+            int a=0;
+            int closedProgramm = 1000;
+            int minNumberCheck = 99;
+            int maxNumberCheck = 999;
+
+            for(int i = 0; i <= closedProgramm; i++)
+            {
+                count += value;
+                if (count >= closedProgramm)
+                {
+                    break;
+                }
+                if (count > minNumberCheck && count <= maxNumberCheck)
+                {
+                    a++;
+                }
+            }
+            Console.WriteLine(a);
+           
+        }
+    }
+}
