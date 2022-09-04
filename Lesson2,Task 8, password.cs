bb6d91af9fc2121eb1e3b336593e7011d05d7686
DmitryMedS <110179038+DmitryMedS@users.noreply.github.com>
2022-09-04 13:05:34 +0300

    add

---
+++ Lesson2,Task 8, password.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int password = 123456;
+            int inputUser;
+            bool isWork = true;
+            int tryCount = 3;
+
+            while (isWork & tryCount > 0)
+            {
+                Console.WriteLine("Введите пароль для доступа к почте: ");
+                inputUser = Convert.ToInt32(Console.ReadLine());
+
+                if (inputUser == password)
+                {
+                    Console.WriteLine("  Пароль введен верно\n  Тайное сообщение...");
+                    isWork = false;
+                }
+                else 
+                {
+                    tryCount--;
+                    Console.WriteLine("Hеверный пароль Осталось " +tryCount+ " попыток.. ");   
+                }
+            }
+        }
+    }
+}
