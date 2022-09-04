1a74fcdc628b1ed4710d3d7832f7ee20eb573479
DmitryMedS <110179038+DmitryMedS@users.noreply.github.com>
2022-09-04 05:05:30 +0300

    Update

--- Lesson2,Task 4.cs
+++ Lesson2,Task 4.cs
@@ -10,7 +10,25 @@ namespace Lesso
     {
         static void Main(string[] args)
         {
-			Console.Write
+			Random random = new Random();
+
+            int minValue = 1;
+            int maxValue = 101;
+            int number = random.Next(minValue,maxValue);
+            int sum = 0;
+            int aliquotNumber = 3;
+            int aliquotNumber2 = 5;
+
+            Console.WriteLine(number);
+
+            for (int i = 0; i < number; i++)
+            {
+                if (i % aliquotNumber == 0 | i % aliquotNumber2 == 0)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine(sum);
 
         }
       }
