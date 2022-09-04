48ddffb74361322bc51894e9d7021c35e0f88af9
DmitryMedS <110179038+DmitryMedS@users.noreply.github.com>
2022-09-04 21:01:31 +0300

    Update

--- Lesson2,Task 1.cs
+++ Lesson2,Task 1.cs
@@ -1,24 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Lesson2.Task1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int countWrite;
-            int isWork = 0;
+            int countRepeat;
+            int number = 0;
             string name = "Дмитрий";
             Console.WriteLine("Сколько раз мне повторить твое имя");
-            countWrite = Convert.ToInt32(Console.ReadLine());
+            countRepeat = Convert.ToInt32(Console.ReadLine());
 
-            while (isWork < countWrite)
+            while (number < countRepeat)
             {
-                isWork++;
+                number++;
                 Console.WriteLine(name);
             }
         }
