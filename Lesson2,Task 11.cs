6039889562aff220b8453fc4a7c9300976a927be
DmitryMedS <110179038+DmitryMedS@users.noreply.github.com>
2022-09-06 05:01:58 +0300

    ///

---
+++ Lesson2,Task 11.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char symbol = '(';
+            string textInput;
+            int nesting = 0;
+            int nesting2 = 0;
+            textInput = Console.ReadLine();
+
+            for(int i = 0; i < textInput.Length; i++)
+            {
+                if (textInput[i] == symbol)
+                {
+                    nesting++;
+                    if (nesting > nesting2)
+                    {
+                        nesting2 = nesting;
+                    }  
+                }
+                else
+                {
+                    nesting--;
+                }
+            }
+            if(nesting == 0)
+            {
+                Console.WriteLine(" все верно " + textInput + " вложенность " + nesting2 + " кратная");
+            }
+            else
+            {
+                Console.WriteLine("НЕ ВЕРНО");
+            }        
+          }
+       }
+    }
+
