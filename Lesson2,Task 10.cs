54a016d86c34ddff15f9c513dd1fa9f1092cb079
DmitryMedS <110179038+DmitryMedS@users.noreply.github.com>
2022-09-11 05:50:14 +0300

    update

--- Lesson2,Task 10.cs
+++ Lesson2,Task 10.cs
@@ -5,19 +5,20 @@ namespace ConsoleApp12
     {
         static void Main(string[] args)
         {
-
             Random rand = new Random();
+            
             int minNumber = 2;
             int maxNumber = 1000;
             int extent = 0;
-            int upExtent = 1;
+            int tempExtent = 1;
             int number = rand.Next(minNumber, maxNumber);
 
-            for (int i =  0; upExtent < number; i++)
+            for (int i =  0; tempExtent < number; i++)
                  {
-                upExtent = upExtent * minNumber;
+                tempExtent = tempExtent * minNumber;
                    extent++;
                  }
+            
             Console.WriteLine(number + " < "+upExtent +"  "+ minNumber + "/" +extent+" степени");}
         }
     }
