54522db5334d249acfef0840dd715b539c46262b
DmitryMedS <110179038+DmitryMedS@users.noreply.github.com>
2022-09-21 06:18:56 +0300

    add

---
+++ Lesson 3, Task 2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace ConsoleApp23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int maxNumber = 0;
+            int minValue = 1;
+            int maxValue = 10;
+            int[,] array = new int[ 10,10];
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(minValue,maxValue);
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i,j]>maxNumber )
+                    {
+                        maxNumber = array[i, j];
+                    }
+                }
+            }
+
+            Console.WriteLine("Максимальное значение массива - "+maxNumber);
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i,j] == maxNumber)
+                    {
+                        array[i, j] = 0;
+                    }
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
