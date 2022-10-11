83c06efd926d8325b0c18850a5a9ade9ec1cab33
DmitryMedS <110179038+DmitryMedS@users.noreply.github.com>
2022-10-11 05:19:09 +0300

    add

---
+++ Lesson 4, Task 1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+
+namespace ConsoleApp34
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isWork = true;
+            string name;
+            string profession;
+            string[] names = {"Шевчук Дмитрий Витальевич" };
+            string[] professions = {"Программист"};
+            int userInput;
+
+            Console.SetCursorPosition(15, 2);
+            Console.WriteLine("Кадровый учет");
+            
+             while (isWork)
+            {           
+                Console.WriteLine("\n\n\n1 - Добавить досье\n\n2 - Показать все досье\n\n3 - Удалить досье\n\n4 - Поиск по фамилии\n\n5 - Выход");
+                Console.WriteLine( "Выберите пункт меню");
+                
+                switch (userInput = Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+
+                        CreateDosier(ref names, ref professions);
+
+                         break;
+                    case 2:
+
+                        for(int i = 0; i < professions.Length; i++)
+                        {
+                            Console.Write((i+1)+" - "+names[i] + " -  "+professions[i] );
+                               Console.WriteLine();
+                        }
+                        break;
+                    case 3:
+
+                        Delete(ref names, ref professions);
+
+                            break;
+                    case 4:
+                        FindDossier(names, professions);
+                        break;
+
+                    case 5:
+                        isWork = false;
+                        break;
+                }
+                
+            }
+        }
+        static void CreateDosier(ref string[] names, ref string[] professions)
+
+        {
+            string name;
+            string profession;
+
+            Console.WriteLine("Добавление досье\nВведите фамилию:\n");
+            name = (" - " + Console.ReadLine() + " - ").ToUpper();
+            Console.WriteLine("Введите Имя:\n");
+            name += (Console.ReadLine() + " - ").ToUpper();
+            Console.WriteLine("Введите Отчество:\n");
+            name += (Console.ReadLine() + " - ").ToUpper();
+            Console.WriteLine("\n  Введите Профессию:\n");
+            profession = (Console.ReadLine()).ToUpper();
+            AddDossier(ref names, name);
+            AddDossier(ref professions, profession);
+            Console.WriteLine("Досье успешно добавлено!");
+            Console.ReadKey();
+
+        }
+        static void AddDossier(ref string[] names, string name)
+        {
+            string[] tempNames = new string[names.Length + 1];
+            for (int i = 0; i < names.Length; i++)
+            {
+                tempNames[i] = names[i];
+            }
+            tempNames[tempNames.Length - 1] = name;
+            names = tempNames;
+           
+            
+            
+        }
+        static void Delete(ref string[] names, ref string[] professions)
+        {
+            int userInput;
+
+            Console.Clear();
+            Console.WriteLine("Ведите номер досье: ");
+            userInput = Convert.ToInt32(Console.ReadLine());
+
+            if (userInput > 0 & userInput <= names.Length)
+            {
+                DecreaseArray(ref names, userInput);
+                DecreaseArray(ref professions, userInput);
+                Console.WriteLine("Досье успешно удалено");
+            }
+            else
+            {
+                Console.WriteLine("Досье не найденоно");
+            }
+            Console.ReadKey();
+        }
+        static void DecreaseArray(ref string[] decrease, int inputData)
+        {
+            string[] tempDecrease = new string[decrease.Length - 1];
+
+            for (int i = 0; i < inputData; i++)
+            {
+                tempDecrease[i] = decrease[i];
+            }
+
+            for (int i = inputData + 1; i < decrease.Length; i++)
+            {
+                tempDecrease[i - 1] = decrease[i];
+            }
+            decrease = tempDecrease; ;
+        }
+        static void FindDossier(string[] fullNames, string[] positions)
+        {
+            string userInput = (Console.ReadLine()).ToUpper();
+            bool isFinded = false;
+            Console.Clear();
+            Console.WriteLine("   Введите фамилию:  \n");
+         
+            foreach (var fullName in fullNames)
+            {
+                if (fullName.Contains(userInput))
+                {
+                    int number = Array.IndexOf(fullNames, fullName);
+                    Console.Clear();
+                    Console.WriteLine($" ID - Фамилия -  Имя - Отчество -  Должность ");
+                    Console.WriteLine($"Досье найдено: {number} - {fullNames[number]} {positions[number]}");
+                    isFinded = true;
+                }
+            }
+
+            if (isFinded == false)
+            {
+                Console.Clear();
+                Console.WriteLine("Досье не найдено!");
+            }
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
---
+++ Lesson 4, Task 2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp35
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int health = 5; int maxHealth = 100;
+            int mana = 3; int maxMana = 100;
+            while (true)
+            {
+                DrawBar(health, maxHealth, ConsoleColor.Red, 0, '#');
+                DrawBar(mana, maxMana, ConsoleColor.Blue, 1, '*');
+                Console.SetCursorPosition(0, 5);
+                Console.Write("Введите % на которое изменянтся жизни ");
+                health += Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите % на которое изменится мана ");
+                mana += Convert.ToInt32(Console.ReadLine());
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+            static void DrawBar(int value, int maxValue, ConsoleColor color, int position, char symbol = ' ')
+            {
+                ConsoleColor defaultColor = Console.BackgroundColor;
+                string bar = "";
+                for (int i = 0; i < value; i++)
+                {
+                    bar += symbol;
+                }
+                Console.SetCursorPosition(0, position);
+                Console.Write("[");
+                Console.BackgroundColor = color;
+                Console.Write(bar);
+                Console.BackgroundColor = defaultColor;
+                bar = "";
+                for (int i = value; i < maxValue; i++)
+                {
+                    bar += ' ';
+
+                }
+                 Console.Write(bar + ']'+value+'%');
+            }
+    }
+}
