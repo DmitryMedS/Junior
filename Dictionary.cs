db07047260f099203036e3550c7d6ea9af4c7056
DmitryMedS <110179038+DmitryMedS@users.noreply.github.com>
2022-11-04 15:55:41 +0300

    add

---
+++ Dictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> words = new Dictionary<string, string>();
+
+            words.Add("черный", "белый");
+            words.Add("сладкий", "соленый");
+            words.Add("большой", "маленький");
+
+            bool isWork = true;
+
+            while (isWork == true)
+            {
+                Console.WriteLine("Введите слово");
+                string word = Console.ReadLine();
+
+                  if (words.ContainsKey(word))
+                    {
+                        Console.WriteLine(words[word]);
+                    }
+
+                   else
+                   {
+                       Console.WriteLine("неверное значение");
+                       isWork = false;
+                       break;
+                   }
+            }
+        }
+    }
+}
+
+
