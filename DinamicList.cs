e26ae1877dd5d3448fe7b1d298b98ec65ac8d5ae
DmitryMedS <110179038+DmitryMedS@users.noreply.github.com>
2022-11-04 17:25:41 +0300

    add

---
+++ DinamicList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+namespace DinamicList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            bool isWork = true;
+            string userInput = null;
+            int number;
+
+            List<int> numbers = new List<int>();
+            Console.WriteLine("Введите число что бы запомнить его\n"+
+                "команда sum -  для вывода суммы чисел\nкоманда exit - выход из программы");
+
+            while (isWork == true)
+            { 
+              userInput = Console.ReadLine();
+                
+                if(userInput == "sum") 
+                { 
+                     for(int i = 0;i < numbers.Count; i++)
+                        {
+                            sum += numbers[i];
+                        }
+
+                        Console.WriteLine("Сумма эллементов = " + sum);
+                }
+                    else if(userInput == "exit")
+                    {
+                    isWork = false;
+                    }
+                else
+                {
+                    numbers.Add(Convert.ToInt32(userInput));
+                }   
+            }
+        }
+    }
+}
+
+
+
